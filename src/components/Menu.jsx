912bd6c6a6e0706e22057ff5f71aa6be8f8de4db
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-09 18:11:10 -0400

    added style-loader and css-loader to devDependencies. created src/css folder with bootstrap.min.css. added css loader to webpack config

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-export default class Menu extends Component {
-  render() {
-    return (
-      <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="patients.html">Patients</a></li>
-        <li><a href="login.html">Login</a></li>
-      </ul>
-    );
-  }
+export default function Menu(props) {
+  return (
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="patients.html">Patients</a></li>
+      <li><a href="login.html">Login</a></li>
+    </ul>
+  );
 }
