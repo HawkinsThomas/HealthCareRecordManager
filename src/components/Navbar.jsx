11907711968f635a49c843d805a2f2c23fdeff48
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-12 13:39:04 -0400

    changed attribute class to className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 export default function Navbar(props) {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">LOGO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">LOGO</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Doctors</a>
+          <li className="nav-item">
+            <a className="nav-link" href="index.html">Doctors</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="patients.html">Patients</a>
+          <li className="nav-item">
+            <a className="nav-link" href="patients.html">Patients</a>
           </li>
         </ul>
       </div>
