10f509de0d83b2f7b835aba496fb6273cf840413
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-14 22:47:06 -0400

    added form actions to patient tables

--- src/pages/patient.jsx
+++ src/pages/patient.jsx
@@ -5,27 +5,57 @@ import Table from 'components/Table';
 //import tableRow from 'components/Table';
 import 'css/bootstrap.min.css';
 
+const formActions = {
+  add: '/',
+  update: '/',
+  delete: '/',
+};
+
 fetch('http://localhost:4000/getPatientInfo/aaaaa-2')
   .then(response => response.json())
   .then((data) => {
     ReactDOM.render(<Navbar />, document.getElementById('menu'));
-    ReactDOM.render(<Table border="1" tableData={data.tableData} tableHeaders={data.tableHeaders} caption="Patient Info" />, document.getElementById('table-patientinfo'));
+    ReactDOM.render(<Table
+      border="1"
+      tableData={data.tableData}
+      tableHeaders={data.tableHeaders}
+      caption="Patient Info"
+      formAction={formActions}
+    />, document.getElementById('table-patientinfo'));
   });
 
-  fetch('http://localhost:4000/getPatientBilling/aaaaa-2')
+fetch('http://localhost:4000/getPatientBilling/aaaaa-2')
   .then(response => response.json())
   .then((data) => {
-    ReactDOM.render(<Table border="1" tableData={data.tableData} tableHeaders={data.tableHeaders} caption="Patient Billing" />, document.getElementById('table-patientbilling'));
+    ReactDOM.render(<Table
+      border="1"
+      tableData={data.tableData}
+      tableHeaders={data.tableHeaders}
+      caption="Patient Billing"
+      formAction={formActions}
+    />, document.getElementById('table-patientbilling'));
   });
 
-  fetch('http://localhost:4000/getPatientImmunization/aaaaa-2')
+fetch('http://localhost:4000/getPatientImmunization/aaaaa-2')
   .then(response => response.json())
   .then((data) => {
-    ReactDOM.render(<Table border="1" tableData={data.tableData} tableHeaders={data.tableHeaders} caption="Patient Immunization" />, document.getElementById('table-patientimmunization'));
+    ReactDOM.render(<Table
+      border="1"
+      tableData={data.tableData}
+      tableHeaders={data.tableHeaders}
+      caption="Patient Immunization"
+      formAction={formActions}
+    />, document.getElementById('table-patientimmunization'));
   });
 
-  fetch('http://localhost:4000/getPatientRevisionHistory/aaaaa-2')
+fetch('http://localhost:4000/getPatientRevisionHistory/aaaaa-2')
   .then(response => response.json())
   .then((data) => {
-    ReactDOM.render(<Table border="1" tableData={data.tableData} tableHeaders={data.tableHeaders} caption="Patient Revision History" />, document.getElementById('table-patientrevisionhistory'));
+    ReactDOM.render(<Table
+      border="1"
+      tableData={data.tableData}
+      tableHeaders={data.tableHeaders}
+      caption="Patient Revision History"
+      formAction={formActions}
+    />, document.getElementById('table-patientrevisionhistory'));
   });
