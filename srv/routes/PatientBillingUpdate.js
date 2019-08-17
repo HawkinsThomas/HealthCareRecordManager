c980fcfa90858973a6ee2c98161fe72ebe0d655b
Yuri Yasvoin <yasvoin@gmail.com>
2019-08-17 14:23:44 -0400

    Modified Update routes

--- srv/routes/PatientBillingUpdate.js
+++ srv/routes/PatientBillingUpdate.js
@@ -1,14 +1,14 @@
-const { getTable } = require('../db/getTable');
+const { writeTable } = require('../db/writeTable');
 
 module.exports = {
   postPatientBillingUpdate: (req, res) => {
-    const health_card_no = req.params.health_card_no;
     const creditcard_no = req.body.creditcard_no;
     const billing_id = req.body.billing_id;
     const cvc = req.body.cvc;
     const card_expiry = req.body.card_expiry;
-    const query = "UPDATE `billing` SET `creditcard_no` = '" + creditcard_no + "', `cvc` = '" + cvc + "', `card_expiry` = '" + card_expiry + "'  WHERE `billing`.`health_card_no` = '" + health_card_no + "' AND `billing`.`billing_id` = '" + billing_id + "'" ;
-    getTable(query)
+    const query = "UPDATE `billing` SET `creditcard_no` = '" + creditcard_no + "', `cvc` = '" + cvc + "', `card_expiry` = '" + card_expiry + "'  WHERE `billing`.`billing_id` = '" + billing_id + "' " ;
+    console.log(query);
+    writeTable(query)
       .then(data => res.json(data));
   },
 };
--- srv/routes/patientImmunizationUpdate.js
+++ srv/routes/patientImmunizationUpdate.js
@@ -1,13 +1,13 @@
-const { getTable } = require('../db/getTable');
+const { writeTable } = require('../db/writeTable');
 
 module.exports = {
   postPatientImmunizationUpdate: (req, res) => {
-    const health_card_no = req.params.health_card_no;
     const immunization_id = req.body.immunization_id;
     const vaccine_name = req.body.vaccine_name;
     const vaccination_date = req.body.vaccination_date;
-    const query = "UPDATE `immunization` SET `vaccine_name` = '" + vaccine_name + "', `vaccination_date` = '" + vaccination_date + "' WHERE `immunization`.`health_card_no` = '" + health_card_no + "' AND `immunization`.`immunization_id` = '" + immunization_id + "'";
-    getTable(query)
+    const query = "UPDATE `immunization` SET `vaccine_name` = '" + vaccine_name + "', `vaccination_date` = '" + vaccination_date + "' WHERE `immunization`.`immunization_id` = '" + immunization_id + "' ";
+    console.log(query);
+    writeTable(query)
       .then(data => res.json(data));
   },
 };
