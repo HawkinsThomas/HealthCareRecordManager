d09dd255b2ff9626834c9af2a4398a0aaccc9fdc
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-17 12:22:52 -0400

    removed billing_id from req.body

--- srv/routes/patientBillingAdd.js
+++ srv/routes/patientBillingAdd.js
@@ -2,12 +2,11 @@ const { getTable } = require('../db/getTable');
 
 module.exports = {
   postPatientBillingAdd: (req, res) => {
-		let health_card_no = req.params.health_card_no;
-		let billing_id = req.body.billing_id;
-        let creditcard_no = req.body.creditcard_no;
-        let cvc = req.body.cvc;
-        let card_expiry = req.body.card_expiry;
-		const query = "INSERT INTO `billing`(billing_id,health_card_no,creditcard_no, cvc, card_expiry) VALUES (NULL,'" + health_card_no + "','" + creditcard_no + "', '" + cvc + "', '" + card_expiry + "')";
+    const health_card_no = req.params.health_card_no;
+    const creditcard_no = req.body.creditcard_no;
+    const cvc = req.body.cvc;
+    const card_expiry = req.body.card_expiry;
+    const query = "INSERT INTO `billing`(billing_id,health_card_no,creditcard_no, cvc, card_expiry) VALUES (NULL,'" + health_card_no + "','" + creditcard_no + "', '" + cvc + "', '" + card_expiry + "')";
     getTable(query)
       .then(data => res.json(data));
   },
