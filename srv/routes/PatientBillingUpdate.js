1e2b9a01d64bc9bac60d87dff317428747a82b18
Yuri Yasvoin <yasvoin@gmail.com>
2019-08-17 12:40:43 -0400

    Modified routes let to const

--- srv/routes/PatientBillingUpdate.js
+++ srv/routes/PatientBillingUpdate.js
@@ -2,12 +2,12 @@ const { getTable } = require('../db/getTable');
 
 module.exports = {
   postPatientBillingUpdate: (req, res) => {
-		let health_card_no = req.params.health_card_no;
-        let creditcard_no = req.body.creditcard_no;
-		let billing_id = req.body.billing_id;
-        let cvc = req.body.cvc;
-        let card_expiry = req.body.card_expiry;
-		const query = "UPDATE `billing` SET `creditcard_no` = '" + creditcard_no + "', `cvc` = '" + cvc + "', `card_expiry` = '" + card_expiry + "'  WHERE `billing`.`health_card_no` = '" + health_card_no + "' AND `billing`.`billing_id` = '" + billing_id + "'" ;
+    const health_card_no = req.params.health_card_no;
+    const creditcard_no = req.body.creditcard_no;
+    const billing_id = req.body.billing_id;
+    const cvc = req.body.cvc;
+    const card_expiry = req.body.card_expiry;
+    const query = "UPDATE `billing` SET `creditcard_no` = '" + creditcard_no + "', `cvc` = '" + cvc + "', `card_expiry` = '" + card_expiry + "'  WHERE `billing`.`health_card_no` = '" + health_card_no + "' AND `billing`.`billing_id` = '" + billing_id + "'" ;
     getTable(query)
       .then(data => res.json(data));
   },
--- srv/routes/patientImmunizationAdd.js
+++ srv/routes/patientImmunizationAdd.js
@@ -2,11 +2,10 @@ const { getTable } = require('../db/getTable');
 
 module.exports = {
   postPatientImmunizationAdd: (req, res) => {
-		let health_card_no = req.params.health_card_no;
-		let immunization_id = req.body.immunization_id;
-        let vaccine_name = req.body.vaccine_name;
-        let vaccination_date = req.body.vaccination_date;
-		const query = "INSERT INTO `immunization`(immunization_id,health_card_no,vaccine_name, vaccination_date) VALUES (NULL,'" + health_card_no + "','" + vaccine_name + "', '" + vaccination_date + "')";
+    const health_card_no = req.params.health_card_no;
+    const vaccine_name = req.body.vaccine_name;
+    const vaccination_date = req.body.vaccination_date;
+    const query = "INSERT INTO `immunization`(immunization_id,health_card_no,vaccine_name, vaccination_date) VALUES (NULL,'" + health_card_no + "','" + vaccine_name + "', '" + vaccination_date + "')";
     getTable(query)
       .then(data => res.json(data));
   },
