8aaf4d32ba1ce41da3d2eb18b9e29ea8c3c2128c
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2019-08-18 11:08:25 -0400

    fixed login

--- srv/routes/login.js
+++ srv/routes/login.js
@@ -4,15 +4,15 @@ const { userExists } = require('../db/userExists');
 
 module.exports = {
   login: (req, res) => {
-    userExists(req.body.username)
+    userExists(req.body.user_name)
       .then((exists) => {
         if (exists) {
-          const query = `SELECT password FROM user WHERE user_name='${req.body.username}';`;
+          const query = `SELECT password FROM user WHERE user_name='${req.body.user_name}';`;
           getTable(query)
             .catch(error => res.json(error))
             .then((data) => {
               if (argon2.verify(data.tableData[0][0], req.body.password)) {
-                req.session.username = req.body.username;
+                req.session.username = req.body.user_name;
                 res.redirect('/');
               } else {
                 res.sendStatus(401);
